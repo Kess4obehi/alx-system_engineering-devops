6d211becadcb5f601a0e5afc144cc9702e1b2635
kess4obehi <kessiena4excellence@gmail.com>
2024-07-11 09:03:02 -0700

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,3 +3,19 @@
 package { 'nginx':
   ensure => installed.
 }
+
+file_line { 'install':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default/',
+  after   => 'listen 80 default_server',
+  line    => 'rewrite ^/redirect_me https://www.github.com/kess4obehi permanent',
+}
+
+file {'/var/www/html/index.html':
+  content  => 'Hello world!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
