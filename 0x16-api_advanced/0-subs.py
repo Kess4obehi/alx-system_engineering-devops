3835eab963ac6d36ba2d737fbbca40b9eb166873
kess4obehi <kessiena4excellence@gmail.com>
2024-08-13 17:25:30 -0700

    Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,10 +17,10 @@ def number_of_subscribers(subreddit):
     header = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'. format(subreddit)
     response = get(url, headers=header, allow_redirects=False)
-    result = response.json()
+    results = response.json()
 
     try:
-        return result.get('data').get('subscribers')
+        return results.get('data').get('subscribers')
 
     except Exception:
         return 0
